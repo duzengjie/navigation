e653a2558798fd33bd9a1ac26732a45956206ae5
我知道了嗯 <dzj0574@aliyun.com>
2023-09-19 10:50:42 +0800

    简化使用方式

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 ![image](https://github.com/duzengjie/navigation/blob/main/file/主界面.png)
 
 # 何如使用
-1. 更改TAB页面 位于Home.vue里面的<el-tab-pane>
-2. 更改每个Tab页面的数据 位于public里面的json文件
+更改每个Tab页面的数据 位于public里面的data.json文件
 
 # 本地运行
 ```shell
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,70 +1,32 @@
 <template>
-    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-        <el-tab-pane label="测试" name="first">
-            <el-row :gutter="20">
-                <el-col :span="6" v-for="item in urls" :key="item.name">
-                    <Card :name="item.name" :remark="item.remark" :url="item.url" />
-                </el-col>
-            </el-row>
-        </el-tab-pane>
-        <el-tab-pane label="模板" name="second">     <el-row :gutter="20">
-                <el-col :span="6" v-for="item in urls2" :key="item.name">
-                    <Card :name="item.name" :remark="item.remark" :url="item.url" />
-                </el-col>
-            </el-row></el-tab-pane>
-        <el-tab-pane label="生产" name="third">     <el-row :gutter="20">
-                <el-col :span="6" v-for="item in urls3" :key="item.name">
-                    <Card :name="item.name" :remark="item.remark" :url="item.url" />
-                </el-col>
-            </el-row></el-tab-pane>
-    </el-tabs>
+  <el-tabs v-model="activeName"  class="tabs"  >
+    <el-tab-pane :label="item.label" :name="item.name" v-for="item in data" :key="item.name">
+      <el-row :gutter="20">
+        <el-col :span="6" v-for="url in item.data" :key="url.name">
+          <Card :name="url.name" :remark="url.remark" :url="url.url" />
+        </el-col>
+      </el-row>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 <script lang="ts" setup>
-import { ref } from 'vue'
-import type { TabsPaneContext } from 'element-plus'
-import Card from "./Card.vue"
-
-const activeName = ref('first')
-const handleClick = (tab: TabsPaneContext, event: Event) => {
-    console.log(tab, event)
-}
-let urls = ref([])
-let urls2 = ref([])
-let urls3 = ref([])
-
-fetch('/testing.json')  // 替换为你的 JSON 文件路径
-    .then(response => response.json())
-    .then(data => {
-            urls.value = data;  // 将获取到的 JSON 数据保存到组件数据中
-        })
-    .catch(error => {
-          console.error('Error:', error);
-        });
-
-fetch('/release.json')  // 替换为你的 JSON 文件路径
-    .then(response => response.json())
-    .then(data => {
-            urls2.value = data;  // 将获取到的 JSON 数据保存到组件数据中
-        })
-    .catch(error => {
-          console.error('Error:', error);
-    });
-
-fetch('/product.json')  // 替换为你的 JSON 文件路径
-    .then(response => response.json())
-    .then(data => {
-            urls3.value = data;  // 将获取到的 JSON 数据保存到组件数据中
-        })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+import { nextTick, ref } from "vue";
+import type { TabsPaneContext } from "element-plus";
+import Card from "./Card.vue";
+import httpService from "../http/request";
+let data = ref([]);
+const activeName = ref('')
+httpService.get("/data.json").then((res) => {
+  data.value = res.data;
+  activeName.value = res.data[0].name
+});
 </script>
 <style>
-.demo-tabs>.el-tabs__content {
-    padding: 10px;
-    color: #6b778c;
-    font-size: 32px;
-    font-weight: 600;
+.tabs > .el-tabs__content {
+  padding: 10px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
 }
 
 .el-row {
@@ -76,6 +38,5 @@ fetch('/product.json')  // 替换为你的 JSON 文件路径
   flex-grow: 1;
   max-width: 100%;
 }
-
 </style>
   
