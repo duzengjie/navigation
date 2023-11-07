174262a13c2eacdfc26c093fca3f29e578437b82
我知道了嗯 <dzj0574@aliyun.com>
2023-11-07 17:28:10 +0800

    新增弹窗

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,70 +1,85 @@
 <template>
-    <el-card class="box-card">
-        <template #header>
-            <div class="card-header">
-                <span><el-text truncated>{{name}}</el-text></span>
-                <el-button class="button" text @click="jump">前往</el-button>
-            </div>
-        </template>
-       <div style="height:50px" @click="open(remark)" > <span class="text item">
-              <el-text truncated>{{remark}}</el-text>
-        </span></div>
-    </el-card>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span
+          ><el-text truncated>{{ name }}</el-text></span
+        >
+        <div>
+          <el-button class="button" type="primary" text @click="jump"
+            >前往</el-button
+          >
+          <el-button class="button" type="success" text @click="updateCard"
+            >更新</el-button
+          >
+        </div>
+      </div>
+    </template>
+    <div style="height: 50px" @click="open(remark)">
+      <span class="text item">
+        <el-text truncated>{{ remark }}</el-text>
+      </span>
+    </div>
+  </el-card>
+
+  <el-dialog v-model="updateCardDialogVisible" title="修改地址">
+    <el-form label-width="120px">
+      <el-form-item label="链接">
+        <el-input v-model="props.url" />
+      </el-form-item>
+
+      <el-form-item label="姓名">
+        <el-input v-model="props.name" />
+      </el-form-item>
+
+      <el-form-item label="备注">
+        <el-input v-model="props.remark" />
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 </template>
 
 
-<script >
-import { defineComponent } from 'vue';
-import { ElMessage, ElMessageBox } from 'element-plus'
-export default defineComponent({
-  props: {
-    url: {
-      type: String
-    },
-    name: {
-      type: String
-    },
-    remark: {
-      type: String
-    }
-  },
-  methods: {
-    jump() {
-      console.log(this.url); // 在方法内部使用 this.url
-      //alert('Hello!');
-      //const newTab = window.open();
-        //newTab.location.href = this.url;
-      window.open(this.url, '_blank')
-    },
-   open(message) {
-   ElMessageBox.alert(message, '备注', {confirmButtonText: 'OK'})
-}
-  }
+<script setup>
+import { defineComponent, ref } from "vue";
+import { ElMessage, ElMessageBox } from "element-plus";
+let updateCardDialogVisible = ref(false)
+
+const props = defineProps({
+  url: String,
+  name: String,
+  remark: String,
 });
-
-
+const jump = () => {
+  window.open(this.url, "_blank")
+};
+const open = (message) => {
+  ElMessageBox.alert(message, "备注", { confirmButtonText: "OK" })
+};
+const updateCard = () => {
+  this.updateCardDialogVisible = true
+}
 </script>
-
 
 <style>
 .card-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 20px;
 }
 
 .text {
-    font-size: 14px;
+  font-size: 14px;
 }
 
 .item {
-    margin-bottom: 18px;
+  margin-bottom: 18px;
 }
 
 .box-card {
-    margin-top: 20px;
-    width: 400px;
+  margin-top: 20px;
+  width: 400px;
 }
 </style>
   
