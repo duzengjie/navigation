2231232b011af78238898b74e2ce4487d7abec08
我知道了嗯 <85781434+duzengjie@users.noreply.github.com>
2023-11-11 10:17:47 +0800

    feat 更新卡片

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -9,7 +9,7 @@
           <el-button class="button" type="primary" text @click="jump"
             >前往</el-button
           >
-          <el-button class="button" type="success" text @click="updateCard"
+          <el-button class="button" type="success" text @click="openUpdateCarddDialog"
             >更新</el-button
           >
         </div>
@@ -22,42 +22,84 @@
     </div>
   </el-card>
 
-  <el-dialog v-model="updateCardDialogVisible" title="修改地址">
-    <el-form label-width="120px">
+  <el-dialog v-model="updateCardDialogVisible" title="修改">
+    <el-form label-width="50px">
       <el-form-item label="链接">
-        <el-input v-model="props.url" />
+        <el-input v-model="form.url" />
       </el-form-item>
 
       <el-form-item label="姓名">
-        <el-input v-model="props.name" />
+        <el-input v-model="form.urlName" />
       </el-form-item>
 
       <el-form-item label="备注">
-        <el-input v-model="props.remark" />
+        <el-input v-model="form.remark" type="textarea"/>
       </el-form-item>
     </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="updateCardDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="updateCard">
+          确认
+        </el-button>
+      </span>
+    </template>
   </el-dialog>
 </template>
 
 
 <script setup>
-import { defineComponent, ref } from "vue";
+import { defineComponent,reactive, ref } from "vue";
 import { ElMessage, ElMessageBox } from "element-plus";
 let updateCardDialogVisible = ref(false)
+import requestService from "../http/request";
+const emits = defineEmits(['refreshParentPage'])
 
 const props = defineProps({
+  id: Number,
   url: String,
   name: String,
   remark: String,
 });
+
+const form = reactive({id:props.id,url:props.url,remark:props.remark,urlName:props.name})
+
+/**
+ * 跳转链接
+ */
 const jump = () => {
   window.open(props.url, "_blank")
 };
+/**
+ * 备注弹出提示
+ * @param {备注} message 
+ */
 const open = (message) => {
   ElMessageBox.alert(message, "备注", { confirmButtonText: "OK" })
 };
-const updateCard = () => {
-  this.updateCardDialogVisible = true
+/**
+ * 打开更新卡片的窗口
+ */
+const openUpdateCarddDialog = () => {
+  updateCardDialogVisible.value = true
+}
+/**
+ * 更新卡片
+ */
+const updateCard = () =>{
+  requestService({
+  url: "/url/api/update",
+  method: 'post',
+  data:{id:form.id,url:form.url,remark:form.remark,urlName:form.urlName}
+}).then((res) => {
+  if(res.data.data == true){
+    ElMessage({message:'更新成功',type:'success'})
+  }else{
+    ElMessage(res.data.msg)
+  }
+  updateCardDialogVisible.value = false
+  emits('refreshParentPage') 
+});
 }
 </script>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,33 +3,40 @@
     <el-tab-pane :label="item.name" :name="item.id" v-for="item in editableTabs" :key="item.id">
       <el-row :gutter="20">
         <el-col :span="6" v-for="url in item.data" :key="url.name">
-          <Card :name="url.urlName" :remark="url.remark" :url="url.url" />
+          <Card :name="url.urlName" :remark="url.remark" :url="url.url" :id="url.id" @refresh-parent-page="getData" />
         </el-col>
       </el-row>
     </el-tab-pane>
   </el-tabs>
 </template>
 <script  setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import Card from "./Card.vue";
 import requestService from "../http/request";
 let tabIndex = 2
+let refresh = reactive({ showRefresh: true })
 //默认选择第一个
 const editableTabsValue = ref(1)
 //标签
 const editableTabs = ref([])
-
+/**
+ * 获取数据方法
+ */
+const getData = () => {
+  requestService({
+    url: "/env/api/list",
+    method: 'get'
+  }).then((res) => {
+    editableTabs.value = res.data.data;
+    //防止id1删除之后 无法默认选择到
+    editableTabsValue.value = res.data.data[0].id
+  });
+}
 /**
  * 初始化数据
  */
-requestService({
-  url: "/env/api/list",
-  method: 'get'
-}).then((res) => {
-  editableTabs.value = res.data.data;
-  //防止id1删除之后 无法默认选择到
-  editableTabsValue.value = res.data.data[0].id
-});
+getData();
+
 
 const handleTabsEdit = (
   targetName,
