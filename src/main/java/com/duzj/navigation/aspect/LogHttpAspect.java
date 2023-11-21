c94de2a4a9c23f9e2b7b1b7ed946a9014cc40ef3
我知道了嗯 <dzj0574@aliyun.com>
2023-11-21 14:40:08 +0800

    feat 日志 http 请求记录

---
+++ src/main/java/com/duzj/navigation/aspect/LogHttpAspect.java
@@ -0,0 +1,57 @@
+package com.duzj.navigation.aspect;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Date 2023/11/21 14:13
+ * @Created by duzengjie
+ */
+@Aspect
+@Slf4j
+@Component
+public class LogHttpAspect {
+
+    private static final List<String> excludeResApi = Lists.newArrayList("downloadAllByExcel");
+    private static final List<String> excludeReqApi = Lists.newArrayList("backupRecoverByExcel");
+
+    @Pointcut("execution(* com.duzj.navigation.controller..*.*(..)))")
+    public void LogHttpAspect(){
+
+    }
+
+    @Around("LogHttpAspect()")
+    public Object doAround(ProceedingJoinPoint joinPoint){
+
+        Object proceed = null;
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+        try {
+            if(!excludeReqApi.contains(name)){
+                Object[] args = joinPoint.getArgs();
+                if(!ObjectUtils.isEmpty(args)){
+                    Object arg = args[0];
+                    log.info("{}接口请求:{}",name, JSONObject.toJSONString(arg));
+                }
+            }
+            proceed = joinPoint.proceed();
+             if(!excludeResApi.contains(name)){
+                 log.info("{}接口返回:{}",name, JSONObject.toJSONString(proceed));
+             }
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+        return proceed;
+    }
+}
