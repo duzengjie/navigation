bc474722534a3bcde214d90c5168fc5739bca8d6
我知道了嗯 <85781434+duzengjie@users.noreply.github.com>
2024-09-15 20:30:41 +0800

    feat 初始化sql更新

--- sql/init.sql
+++ sql/init.sql
@@ -1,47 +1,68 @@
 CREATE DATABASE `navigation` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
--- navigation.environment_info definition
+/*
+ Navicat Premium Dump SQL
 
-CREATE TABLE `environment_info` (
-                                    `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-                                    `name` varchar(20) NOT NULL COMMENT '环境名称',
-                                    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-                                    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-                                    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='环境信息';
+ Source Server         : 127.0.0.1·duzengjie
+ Source Server Type    : MySQL
+ Source Server Version : 80300 (8.3.0)
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : navigation
 
+ Target Server Type    : MySQL
+ Target Server Version : 80300 (8.3.0)
+ File Encoding         : 65001
 
--- navigation.url_info definition
+ Date: 15/09/2024 20:28:05
+*/
 
-CREATE TABLE `url_info` (
-                            `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-                            `environment_id` int(11) NOT NULL COMMENT '环境 id',
-                            `url` varchar(255) NOT NULL COMMENT '地址',
-                            `remark` varchar(255) DEFAULT NULL COMMENT '备注',
-                            `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-                            `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-                            `url_name` varchar(100) NOT NULL COMMENT 'url名称',
-                            PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='链接信息';
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- 01.08 新增
-ALTER TABLE navigation.url_info ADD order_num int NULL COMMENT '排序,数字越小展示越靠前';
-ALTER TABLE navigation.url_info CHANGE order_num order_num int NULL COMMENT '排序,数字越小展示越靠前' AFTER remark;
--- navigation.url_info_change_log definition
-CREATE TABLE `url_info_change_log` (
-                                       `id` int NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-                                       `original_id` int NOT NULL COMMENT '原 id',
-                                       `environment_id` int NOT NULL COMMENT '环境 id',
-                                       `url_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'url名称',
-                                       `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '地址',
-                                       `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '备注',
-                                       `order_num` int DEFAULT NULL COMMENT '排序,数字越小展示越靠前',
-                                       `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-                                       PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='链接信息修改记录';
--- 01.10 新增 使用次数
+-- ----------------------------
+-- Table structure for environment_info
+-- ----------------------------
+DROP TABLE IF EXISTS `environment_info`;
+CREATE TABLE `environment_info`  (
+                                     `id` int NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+                                     `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '环境名称',
+                                     `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+                                     `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+                                     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '环境信息' ROW_FORMAT = Dynamic;
 
-ALTER TABLE navigation.url_info ADD use_num int NULL COMMENT '使用次数';
-ALTER TABLE navigation.url_info CHANGE use_num use_num int NULL COMMENT '使用次数' AFTER order_num;
+-- ----------------------------
+-- Table structure for url_info
+-- ----------------------------
+DROP TABLE IF EXISTS `url_info`;
+CREATE TABLE `url_info`  (
+                             `id` int NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+                             `environment_id` int NOT NULL COMMENT '环境 id',
+                             `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '地址',
+                             `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
+                             `order_num` int NULL DEFAULT NULL COMMENT '排序,数字越小展示越靠前',
+                             `use_num` int NULL DEFAULT NULL COMMENT '使用次数',
+                             `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+                             `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+                             `url_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'url名称',
+                             PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '链接信息' ROW_FORMAT = Dynamic;
 
-update url_info  set use_num = 0;
+-- ----------------------------
+-- Table structure for url_info_change_log
+-- ----------------------------
+DROP TABLE IF EXISTS `url_info_change_log`;
+CREATE TABLE `url_info_change_log`  (
+                                        `id` int NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+                                        `original_id` int NOT NULL COMMENT '原 id',
+                                        `environment_id` int NOT NULL COMMENT '环境 id',
+                                        `url_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'url名称',
+                                        `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '地址',
+                                        `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
+                                        `order_num` int NULL DEFAULT NULL COMMENT '排序,数字越小展示越靠前',
+                                        `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+                                        PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '链接信息修改记录' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
